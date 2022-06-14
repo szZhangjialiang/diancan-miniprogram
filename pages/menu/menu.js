1bfb0a2400e21b65b1eea87fcae32fa08c2d83d5
zhangjialiang <361701463@qq.com>
2022-06-14 17:22:25 +0800

    updata som feature

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -2,10 +2,21 @@ const db = wx.cloud.database()
 Page({
   data: {
     cafeList: [],
-    number: 0,
   },
   add(e) {
    console.log(e) 
+   let index = e.currentTarget.dataset.index
+   console.log('这是index',index)
+   let carts = this.data.cafeList
+   let quantity = carts[index].quantity
+   quantity = quantity + 1
+   carts[index].quantity = quantity
+   console.log('这是quantity',quantity)
+
+   this.setData({
+     cafeList : carts
+   })
+   console.log('修改quantity后',this.data.cafeList)
   },
   onLoad() {
     db.collection('cafe').get()
